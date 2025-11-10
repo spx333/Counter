0275b000911e67e227c994a5707a65f52fca676f
spx333 <spx3@ya.ru>
2025-11-10 20:33:12 +0300

    Создан проект

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Сергей Петров on 10.11.2025.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
