55cee422c49d58a8abc17bdc909492809f8ef835
spx333 <spx3@ya.ru>
2025-11-11 19:43:48 +0300

    Сделана задача спринт 3 (2 версия)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var logsTextView: UITextView!
+    
+    var result: Int = 0
+
+    let dateFormatter = DateFormatter()
+
+  
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
+    }
+    
+    
+    func scrollTextViewToBottom() {
+        if logsTextView.text.count > 0 {
+               let location = logsTextView.text.count - 1
+               let bottom = NSMakeRange(location, 1)
+               logsTextView.scrollRangeToVisible(bottom)
+           }
+        
     }
 
-
+    @IBAction func zeroButton() {
+        let date = Date()
+        result=0
+        resultLabel.text = "Значение счётчика: \(result)"
+        logsTextView.text=logsTextView.text+"\n[\(dateFormatter.string(from: date))]: значение сброшено"
+        scrollTextViewToBottom()
+    }
+    
+    @IBAction func removeButton() {
+        let date = Date()
+        if result>0 {
+            result-=1
+            logsTextView.text=logsTextView.text+"\n[\(dateFormatter.string(from: date))]: значение изменено на -1"
+            scrollTextViewToBottom()
+        }
+        else {
+            logsTextView.text=logsTextView.text+"\n[\(dateFormatter.string(from: date))]: попытка уменьшить значение счётчика ниже 0"
+            scrollTextViewToBottom()
+        }
+        resultLabel.text = "Значение счётчика: \(result)"
+        
+    }
+    
+    @IBAction func addButton() {
+        let date = Date()
+        result+=1
+        resultLabel.text = "Значение счётчика: \(result)"
+        logsTextView.text=logsTextView.text+"\n[\(dateFormatter.string(from: date))]: значение изменено на +1"
+        scrollTextViewToBottom()
+    }
 }
 
